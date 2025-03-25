bb838a53a89fd2df6ffe6a57f9c9f20ae777a7c9
Denis Volnenko <denis@volnenko.ru>
2025-03-25 19:58:38 +0400

    Project updated.

--- src/main/java/ru/volnenko/plugin/openapidoc/Generator.java
+++ src/main/java/ru/volnenko/plugin/openapidoc/Generator.java
@@ -1,4 +1,67 @@
 package ru.volnenko.plugin.openapidoc;
 
-public class Generator {
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Mojo(name = "generate", defaultPhase = LifecyclePhase.COMPILE)
+public class Generator extends AbstractMojo {
+
+    @Getter
+    @Setter
+    @Parameter(property = "serviceName")
+    public String serviceName = "Сервис";
+
+    @Getter
+    @Setter
+    @Parameter(property = "dataBaseInfo")
+    public String dataBaseInfo = "";
+
+    @Getter
+    @Setter
+    @Parameter(property = "headerFirstEnabled")
+    public boolean headerFirstEnabled = true;
+
+    @Getter
+    @Setter
+    @Parameter(property = "headerSecondEnabled")
+    public boolean headerSecondEnabled = true;
+
+    @Getter
+    @Setter
+    @Parameter(property = "tableOfContentsEnabled")
+    public boolean tableOfContentsEnabled = true;
+
+    @Getter
+    @Setter
+    @Parameter(property = "outputPath")
+    public String outputPath = "./doc";
+
+    @Getter
+    @Setter
+    @Parameter(property = "outputFile")
+    public String outputFile = "index.adoc";
+
+    @Parameter(defaultValue = "${project}", required = true, readonly = true)
+    private MavenProject project;
+
+    @Getter
+    @Setter
+    @Parameter(property = "files")
+    private List<String> files = new ArrayList<>();
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+
+    }
+
 }
