14a35694b5edc78cc8776f48fa3c9ae880c0427a
Denis Volnenko <denis@volnenko.ru>
2025-04-02 09:52:40 +0400

    Project updated.

--- git-pull-push.sh
+++ git-pull-push.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 git pull
-git pull git@github.com:denis-volnenko/maven-openapi-asciidoc-plugin.git
+git pull git@github.com:denis-volnenko/openapi-asciidoc-maven-plugin.git
 
 git add .
 git commit -m "Project updated."
 
 git push
 
-git push git@github.com:denis-volnenko/maven-openapi-asciidoc-plugin.git
+git push git@github.com:denis-volnenko/openapi-asciidoc-maven-plugin.git
