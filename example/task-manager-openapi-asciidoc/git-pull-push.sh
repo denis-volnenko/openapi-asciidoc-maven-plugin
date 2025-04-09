ab680ce64a85d06d669d85c81b536b25583cb253
Denis Volnenko <denis@volnenko.ru>
2025-04-09 20:20:03 +0400

    Project updated.

--- example/task-manager-openapi-asciidoc/git-pull-push.sh
+++ example/task-manager-openapi-asciidoc/git-pull-push.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 git pull
+git pull git@github.com:denis-volnenko/openapi-asciidoc-maven-plugin.git
 
 git add .
 git commit -m "Task Manager updated."
 
 git push
-
-git push git@github.com:denis-volnenko/maven-openapi-asciidoc-plugin.git
+git push git@github.com:denis-volnenko/openapi-asciidoc-maven-plugin.git
--- git-pull-push.sh
+++ git-pull-push.sh
@@ -7,5 +7,4 @@ git add .
 git commit -m "Project updated."
 
 git push
-
 git push git@github.com:denis-volnenko/openapi-asciidoc-maven-plugin.git
