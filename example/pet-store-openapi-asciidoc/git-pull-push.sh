d19eab466708cfcadf2988ab1bc6964e57d6a28a
Denis Volnenko <denis@volnenko.ru>
2025-04-09 20:21:14 +0400

    pet-store-openapi-asciidoc updated.

--- example/pet-store-openapi-asciidoc/git-pull-push.sh
+++ example/pet-store-openapi-asciidoc/git-pull-push.sh
@@ -4,7 +4,7 @@ git pull
 git pull git@github.com:denis-volnenko/openapi-asciidoc-maven-plugin.git
 
 git add .
-git commit -m "Task Manager updated."
+git commit -m "pet-store-openapi-asciidoc updated."
 
 git push
 git push git@github.com:denis-volnenko/openapi-asciidoc-maven-plugin.git
