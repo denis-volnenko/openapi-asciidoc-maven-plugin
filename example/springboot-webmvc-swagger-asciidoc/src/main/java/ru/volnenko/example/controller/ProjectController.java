034ebd0a03ccf2b1f3d700c4c2d1420b4c9e773d
Denis Volnenko <denis@volnenko.ru>
2025-04-10 10:31:10 +0400

    springboot-webmvc-swagger-asciidoc updated.

--- example/springboot-webmvc-swagger-asciidoc/src/main/java/ru/volnenko/example/controller/ProjectController.java
+++ example/springboot-webmvc-swagger-asciidoc/src/main/java/ru/volnenko/example/controller/ProjectController.java
@@ -1,7 +1,10 @@
 package ru.volnenko.example.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.volnenko.example.model.ProjectDTO;
@@ -14,9 +17,19 @@ import java.util.List;
 @RequestMapping("/api/v1/project")
 public class ProjectController {
 
+//    @ApiOperation("Получение всех проектов")
     @GetMapping(value = "/all", produces = "application/json")
     public List<ProjectDTO> selectAllProject() {
         return Collections.emptyList();
+    }
+
+//    @ApiOperation("Получение проекта по Id")
+    @GetMapping(value = "/one/{id}")
+    public ProjectDTO selectOneProject(
+//            @ApiParam(name = "Идентификатор")
+            @PathVariable("id") String id
+    ) {
+        return new ProjectDTO();
     }
 
 }
--- example/springboot-webmvc-swagger-asciidoc/src/main/java/ru/volnenko/example/controller/TaskController.java
+++ example/springboot-webmvc-swagger-asciidoc/src/main/java/ru/volnenko/example/controller/TaskController.java
@@ -1,7 +1,10 @@
 package ru.volnenko.example.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.volnenko.example.model.TaskDTO;
@@ -14,9 +17,19 @@ import java.util.List;
 @RequestMapping("/api/v1/task")
 public class TaskController {
 
+//    @ApiOperation("Получение всех задач")
     @GetMapping(value = "/all", produces = "application/json")
     public List<TaskDTO> selectAllTask() {
         return Collections.emptyList();
+    }
+
+//    @ApiOperation("Получение задачи по Id")
+    @GetMapping(value = "/one/{id}")
+    public TaskDTO selectOneTask(
+//            @ApiParam(name = "Идентификатор")
+            @PathVariable("id") String id
+    ) {
+        return new TaskDTO();
     }
 
 }
