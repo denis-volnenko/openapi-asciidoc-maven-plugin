7cf84f2ddc61c520996965eba887b7718b03ceb7
Denis Volnenko <denis@volnenko.ru>
2025-04-10 10:11:50 +0400

    springboot-webmvc-swagger-asciidoc updated.

--- example/springboot-webmvc-swagger-asciidoc/src/main/java/ru/volnenko/example/model/DateDTO.java
+++ example/springboot-webmvc-swagger-asciidoc/src/main/java/ru/volnenko/example/model/DateDTO.java
@@ -1,12 +1,19 @@
 package ru.volnenko.example.model;
 
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import java.util.Date;
 
 /**
  * @author Denis Volnenko
  */
+
+@ApiModel(description = "Класс для передачи даты и времени")
 public class DateDTO {
 
+    @ApiModelProperty("Дата/Время")
     private Date date;
 
     public DateDTO(Date date) {
