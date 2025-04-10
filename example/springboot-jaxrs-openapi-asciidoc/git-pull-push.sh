884594c030e39ecc8023da49e12582ef21cde9d6
Denis Volnenko <denis@volnenko.ru>
2025-04-10 08:22:50 +0400

    springboot-jaxrs-openapi-asciidoc updated.

--- example/springboot-jaxrs-openapi-asciidoc/git-pull-push.sh
+++ example/springboot-jaxrs-openapi-asciidoc/git-pull-push.sh
@@ -4,7 +4,7 @@ git pull
 git pull git@github.com:denis-volnenko/openapi-asciidoc-maven-plugin.git
 
 git add .
-git commit -m "springboot-openapi-asciidoc updated."
+git commit -m "springboot-jaxrs-openapi-asciidoc updated."
 
 git push
 git push git@github.com:denis-volnenko/openapi-asciidoc-maven-plugin.git
